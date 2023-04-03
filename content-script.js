ee14285d2289608d3fef8273e2d64f6f75ffc674
Ming Tam <ming@fotins.com>
2023-04-03 16:45:46 +0800

    Mark "+++" next to the manga title I want
    It is surprisingly easy.

--- content-script.js
+++ content-script.js
@@ -1,3 +1,8 @@
+function mark_text(element) 
+{
+    element.innerText = "+++" + element.innerText;
+}
+
 console.log("content-script.js start");
 
 // var someText = document.body.textContent;
@@ -8,13 +13,13 @@ console.log("content-script.js start");
 
 console.log(location.host);
 console.log(location.hostname);
+
 if (location.host === "example.com")
 {
     var someText = document.body.getElementsByTagName("h1");
     console.log(someText);
 
     document.body.style.backgroundColor = "yellow";
-    // document.body.textContent = document.body.textContent.replace("Example", "<u>Example</u>");
     var targetHTML = document.body.getElementsByTagName("h1")[0].innerHTML;
     document.body.getElementsByTagName("h1")[0].innerHTML = targetHTML.replace("Example", "<u>Example</u>");
 
@@ -22,5 +27,34 @@ if (location.host === "example.com")
     var someText = document.body.getElementsByTagName("h1");
     console.log(someText);
 
-    console.log("content-script.js end");
 }
+
+if (location.host === "www.manhuagui.com")
+{
+    const manga = [
+        "影宅",
+        "旅馆生活・吸血鬼",
+        "如积雪般的永寂",
+        "葬送者芙莉莲",
+        "琉璃Dragon",
+        "说着「请将我的孩子杀死」的父母们",
+        "飞翔的魔女",
+        "吃货女仆",
+        "笔与手铐与事实婚姻",
+        "迷宫饭",
+        "JK除灵师小茜",
+    ];
+
+    // document.body.style.backgroundColor = "blue";
+    ell = document.getElementsByClassName("ell")
+    console.log(ell[0].innerText);
+    for (let i in ell)
+    {
+        if (manga.includes(ell[i].innerText))
+        {
+            ell[i].innerText = "+++" + ell[i].innerText;
+        }
+    }
+}
+
+console.log("content-script.js end");
