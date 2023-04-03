b6acb8a4ea38464b3d6fc0770c5847538d27dc50
Ming Tam <ming@fotins.com>
2023-04-03 16:06:34 +0800

    Do host matching in content-script

--- content-script.js
+++ content-script.js
@@ -6,16 +6,21 @@ console.log("content-script.js start");
 // var someText = document.body.innerHTML;
 // console.log(someText);
 
-var someText = document.body.getElementsByTagName("h1");
-console.log(someText);
+console.log(location.host);
+console.log(location.hostname);
+if (location.host === "example.com")
+{
+    var someText = document.body.getElementsByTagName("h1");
+    console.log(someText);
 
-document.body.style.backgroundColor = "yellow";
-// document.body.textContent = document.body.textContent.replace("Example", "<u>Example</u>");
-var targetHTML = document.body.getElementsByTagName("h1")[0].innerHTML;
-document.body.getElementsByTagName("h1")[0].innerHTML = targetHTML.replace("Example", "<u>Example</u>");
+    document.body.style.backgroundColor = "yellow";
+    // document.body.textContent = document.body.textContent.replace("Example", "<u>Example</u>");
+    var targetHTML = document.body.getElementsByTagName("h1")[0].innerHTML;
+    document.body.getElementsByTagName("h1")[0].innerHTML = targetHTML.replace("Example", "<u>Example</u>");
 
-console.log("======");
-var someText = document.body.getElementsByTagName("h1");
-console.log(someText);
+    console.log("======");
+    var someText = document.body.getElementsByTagName("h1");
+    console.log(someText);
 
-console.log("content-script.js end");
+    console.log("content-script.js end");
+}
