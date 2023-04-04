7cc1d4f97e8e06a3405253cfa3b5a66f5be3cdbf
Ming Tam <ming@fotins.com>
2023-04-04 11:00:50 +0800

    Able to save and load settings

---
+++ options.js
@@ -0,0 +1,32 @@
+// Saves options to chrome.storage
+const saveOptions = () => {
+    const color = document.getElementById('color').value;
+    const likesColor = document.getElementById('like').checked;
+    const settingJSON = document.getElementById('setting_json').value;
+
+    chrome.storage.sync.set(
+        { settingJSON: settingJSON },
+        () => {
+            // Update status to let user know options were saved.
+            const status = document.getElementById('status');
+            status.textContent = 'Options saved.';
+            setTimeout(() => {
+                status.textContent = '';
+            }, 750);
+        }
+    );
+};
+
+// Restores select box and checkbox state using the preferences
+// stored in chrome.storage.
+const restoreOptions = () => {
+    chrome.storage.sync.get(
+        { settingJSON: "Setting in JSON" },
+        (items) => {
+            document.getElementById('setting_json').value = items.settingJSON;
+        }
+    );
+};
+
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.getElementById('save').addEventListener('click', saveOptions);
